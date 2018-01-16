f523cbf4463e50333c2e112f8a5d1aa2b0938bcb
Michael Herman <mwherman2000@hotmail.com>
2018-01-16 13:22:17 -0500

    Initial checkin

---
+++ MWH.MyNethereum.TDownload/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MWH.MyNethereum.TDownload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press Enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
