f93c76b14aa55c197a4635ca29cfec186f7fbe75
Michael Herman (Lenovo) <mwherman@parallelspace.net>
2018-02-01 09:00:56 -0500

    CsvBulkCopyDataIntoSqlServer (rename)

---
+++ EtherToSQL1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EtherToSQL1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string queryString = "SELECT OrderID, CustomerID FROM dbo.Orders;";
+
+            using (SqlConnection connection =
+                       new SqlConnection(connectionString))
+            {
+                SqlCommand command =
+                    new SqlCommand(queryString, connection);
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                // Call Read before accessing data.
+                while (reader.Read())
+                {
+                    ReadSingleRow((IDataRecord)reader);
+                }
+
+                // Call Close when done reading.
+                reader.Close();
+            }
+        }
+    }
+}
