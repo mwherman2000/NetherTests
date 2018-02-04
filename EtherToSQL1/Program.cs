219019380eb5ce119edadd7086de70c28a3e6cd8
Michael Herman (Lenovo) <mwherman@parallelspace.net>
2018-02-03 19:59:12 -0500

    Pricing worked

--- EtherToSQL1/Program.cs
+++ EtherToSQL1/Program.cs
@@ -14,23 +14,23 @@ namespace EtherToSQL1
 {
     class Program
     {
+        static string _startAddress = "0x0e7d6241eef945b57229b590f2611c234a8b7c74";
         static string _serverName = @"DESKTOP-ETTAF0H";
         static string _databaseName = "a-test3"; // "a-test4D"; // "a-test3";
 
         static string _AMTableName = "AddressMaster2";
         static string _ABTableName = "AddressBalances2";
 
-        static int _batchSize = 1000;
+        static int _batchSize = 100000;
 
         static string _connectionString = String.Format(@"Data Source={0};Initial Catalog={1};Integrated Security=True;MultipleActiveResultSets=True;Connection Timeout=120",
                                                          _serverName, _databaseName);
         static string _queryABTop1 = String.Format("SELECT TOP 1 * FROM {0};", _ABTableName);
-        static string _queryAMAll = String.Format("SELECT [Address], [Type] FROM [{0}] WHERE [Address] > '0x0ab41f5bd491413b53aeda1b4ee61eb1dcd0dc35' ORDER BY [Address], [Type]", _AMTableName);
+
+        static string _templateAMTopN = "SELECT TOP {0} [Address] FROM [{1}] WHERE [Address] > '{2}' ORDER BY [Address] ASC";
+        //static string _queryAMAll = String.Format("SELECT [Address] FROM [{0}] WHERE [Address] > '0x0ccae2de09df18be7a50337cab58d6bd35954246' ORDER BY [Address]", _AMTableName);
         //static string _templateAMOffsetFetch = "SELECT [Address], [Type] FROM [{0}] ORDER BY [Address], [Type] " +
         //                                        "OFFSET {1} ROWS FETCH NEXT {2} ROWS ONLY;";
-
-        static DataTable dtAM = null;
-        static DataTable dtAB = null;
 
         static void Main(string[] args)
         {
@@ -69,44 +69,55 @@ namespace EtherToSQL1
                 /* https://stackoverflow.com/questions/38828943/does-sqldataadapterfill-fetch-the-whole-result-set-or-is-it-on-demand */
 
                 int offset = 0;
-                Console.WriteLine("_queryAMAll: " + _queryAMAll);
-                SqlDataAdapter daAMLoader = new SqlDataAdapter(_queryAMAll, connection);
+                string _queryAMTopN = String.Format(_templateAMTopN, _batchSize, _AMTableName, _startAddress);
+                Console.WriteLine("_queryAMTopN: " + _queryAMTopN);
+                SqlDataAdapter daAMLoader = new SqlDataAdapter(_queryAMTopN, connection);
                 dtAM.Rows.Clear();
-                rows = daAMLoader.Fill(offset, _batchSize, dtAM);
+                rows = daAMLoader.Fill(dtAM);
                 while (rows > 0)
                 {
-                    Console.Write("." + offset.ToString());
+                    Console.Write(offset.ToString() + ">");
 
                     // Create a batch of rows
                     dtAB.Rows.Clear();
+                    string addrLast = "";
                     foreach (DataRow drAM in dtAM.Rows)
                     {
-                        string addr = drAM[0].ToString();
+                        addrLast = drAM[0].ToString();
 
-                        Task<HexBigInteger> t = GetAccountBalanceWei(addr);
+                        Task<HexBigInteger> t = GetAccountBalanceWei(addrLast);
                         t.Wait(10000);
                         var hexWei = t.Result;
                         decimal balanceWei = Web3.Convert.FromWei(hexWei.Value, UnitConversion.EthUnit.Wei);
                         decimal balanceEther = Web3.Convert.FromWei(hexWei.Value, UnitConversion.EthUnit.Ether);
 
                         DataRow drAB = dtAB.NewRow();
-                        drAB[0] = addr;
+                        drAB[0] = addrLast;
                         drAB[1] = DateTime.UtcNow;
                         drAB[2] = balanceWei;
                         drAB[3] = balanceEther;
                         dtAB.Rows.Add(drAB);
                     }
+                    Console.Write(offset.ToString() + "}");
 
                     // Bulk insert batch of rows
                     sqlBulkCopy.WriteToServer(dtAB);
+                    Console.Write(offset.ToString() + "]");
 
-                    dtAM.Rows.Clear();
                     offset += _batchSize;
-                    rows = daAMLoader.Fill(offset, _batchSize, dtAM);
+
+                    _queryAMTopN = String.Format(_templateAMTopN, _batchSize, _AMTableName, addrLast);
+                    Console.WriteLine("_queryAMTopN: " + _queryAMTopN);
+                    if (daAMLoader != null) daAMLoader.Dispose();
+                    daAMLoader = new SqlDataAdapter(_queryAMTopN, connection);
+                    dtAM.Rows.Clear();
+                    rows = daAMLoader.Fill(dtAM);
                 }
                 Console.WriteLine("|");
                 connection.Close();
             }
+            Console.WriteLine("Press Enter to exit...");
+            Console.ReadLine();
         }
 
         static public async Task<HexBigInteger> GetAccountBalanceWei(string addr)
@@ -138,9 +149,9 @@ namespace EtherToSQL1
         static Web3 GetWeb3Client()
         {
             //Web3 web3 = new Web3();
-            Web3 web3 = new Web3("https://mainnet.infura.io/hZeiirtHOLO11uuyLySi");
-            //var ipcClient = new Nethereum.JsonRpc.IpcClient.IpcClient("./geth.ipc");
-            //Web3 web3 = new Web3(ipcClient);
+            //Web3 web3 = new Web3("https://mainnet.infura.io/hZeiirtHOLO11uuyLySi");
+            var ipcClient = new Nethereum.JsonRpc.IpcClient.IpcClient("./geth.ipc");
+            Web3 web3 = new Web3(ipcClient);
             return web3;
         }
     }
